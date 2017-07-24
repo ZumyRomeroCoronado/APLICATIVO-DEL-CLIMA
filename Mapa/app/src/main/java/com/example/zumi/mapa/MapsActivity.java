9395b7d5f3962870f0ad1c5fa8231020f4d61b6d
ZumyRomeroCoronado <zumy.epis@gmail.com>
2017-07-24 08:53:32 -0500

    abansando

--- Mapa/app/src/main/java/com/example/zumi/mapa/MapsActivity.java
+++ Mapa/app/src/main/java/com/example/zumi/mapa/MapsActivity.java
@@ -5,10 +5,13 @@ import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -33,7 +36,7 @@ import java.io.InputStreamReader;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    LatLng lima=new LatLng(-8.3751034,-74.6517595);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,56 +61,60 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        String Ciudades[] ={"Espinar","camaná","nazca"
-                ,"chachapoyas","huaraz","abancay","arequipa","ayacucho","cajamarca","callao","andahuaylas","cusco","huancavelica",
-                "huanuco","ica","huancayo","trujillo","chiclayo","lima","iquitos","puerto%20maldonado",
-                "moquegua","cerro%20de%20pasco","piura","puno","moyobamba","tacna","tumbes","pucallpa","puquio","bagua"};
+        LatLng lima = new LatLng(-8.3751034, -74.6517595);
+
+        String Ciudades[] = {"Espinar", "camaná", "nazca"
+                , "chachapoyas", "huaraz", "abancay", "arequipa", "ayacucho", "cajamarca", "callao", "andahuaylas", "cusco", "huancavelica",
+                "huanuco", "ica", "huancayo", "trujillo", "chiclayo", "lima", "iquitos", "puerto%20maldonado",
+                "moquegua", "cerro%20de%20pasco", "piura", "puno", "moyobamba", "tacna", "tumbes", "pucallpa", "puquio", "bagua"};
         // Add a marker in Sydney and move the camera
-       // LatLng sydney = new LatLng(-34, 151);//.................
+        // LatLng sydney = new LatLng(-34, 151);//.................
 
 
-        for( int i=0; i<31; i++)
-        {
+        for (int i = 0; i < 31; i++) {
             new ReadJSONFeedTask().execute(
-                    "http://api.openweathermap.org/data/2.5/weather?q="+Ciudades[i]+",PE&units=metric&APPID=9c390ebbe3276a74e74814962661b624");
+                    "http://api.openweathermap.org/data/2.5/weather?q=" + Ciudades[i] + ",PE&units=metric&APPID=9c390ebbe3276a74e74814962661b624");
 
         }
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lima,5));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lima, 5));
     }
-/**llamamos el url para q  lea la lon lan*/
-public String readJSONFeed(String URL ){
-    StringBuilder stringBuilder = new StringBuilder();
 
-    HttpClient client = new DefaultHttpClient();
-    HttpGet httpGet = new HttpGet(URL);
-    try {
-        HttpResponse response = client.execute(httpGet);
-        StatusLine statusLine = response.getStatusLine();
-        int statusCode = statusLine.getStatusCode();
-        if (statusCode == 200) {
-            HttpEntity entity = response.getEntity();
-            InputStream content = entity.getContent();
-            BufferedReader reader = new BufferedReader(
-                    new InputStreamReader(content));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                stringBuilder.append(line);
+    /**
+     * llamamos el url para q  lea la lon lan
+     */
+    public String readJSONFeed(String URL) {
+        StringBuilder stringBuilder = new StringBuilder();
+        HttpClient client = new DefaultHttpClient();
+        HttpGet httpGet = new HttpGet(URL);
+        try {
+            HttpResponse response = client.execute(httpGet);
+            StatusLine statusLine = response.getStatusLine();
+            int statusCode = statusLine.getStatusCode();
+            if (statusCode == 200) {
+                HttpEntity entity = response.getEntity();
+                InputStream content = entity.getContent();
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(content));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line);
+                }
+            } else {
+                Log.e("JSON", "Failed to download file");
             }
-        } else {
-            Log.e("JSON", "Failed to download file");
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-    } catch (ClientProtocolException e) {
-        e.printStackTrace();
-    } catch (IOException e) {
-        e.printStackTrace();
+        return stringBuilder.toString();
     }
-    return stringBuilder.toString();
-}
 
     private class ReadJSONFeedTask extends AsyncTask<String, Void, String> {
         protected String doInBackground(String... urls) {
             return readJSONFeed(urls[0]);
         }
+
         protected void onPostExecute(String result) {
 
             try {
@@ -115,27 +122,24 @@ public String readJSONFeed(String URL ){
                 JSONObject obj1 = obj.getJSONObject("main");
                 JSONObject objcoor = obj.getJSONObject("coord");
 
-                Float latitud=Float.parseFloat(objcoor.getString("lat"));
-                Float longitud=Float.parseFloat(objcoor.getString("lon"));
+                Float latitud = Float.parseFloat(objcoor.getString("lat"));
+                Float longitud = Float.parseFloat(objcoor.getString("lon"));
                 String temperatura = obj1.getString("temp");
                 String ciudad = obj.getString("name");
                 String humedad = obj1.getString("humidity");
-                LatLng coordenada= new LatLng(latitud,longitud);
+                LatLng coordenada = new LatLng(latitud, longitud);
 
-                Marker ubicacion=mMap.addMarker(new MarkerOptions().position(coordenada).title("Ciudad : "+ciudad)
-                        .snippet("T : "+temperatura+" C° ; H :"+humedad+"%"));
+                Marker ubicacion = mMap.addMarker(new MarkerOptions().position(coordenada).title("Ciudad : " + ciudad)
+                        .snippet("T : " + temperatura + " C° ; H :" + humedad + "%"));
 
                 ubicacion.showInfoWindow();
 
-            }
-            catch (Exception e) {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
-
 
 
         }
 
     }
-
 }
